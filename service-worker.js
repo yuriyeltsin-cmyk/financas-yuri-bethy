217a0f669f547a50ae3253c7abecc2bc2bf29662
yuriyeltsin-cmyk <yuriyeltsin@gmail.com>
2026-08-24 22:05:00 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-const CACHE='financas-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
+const CACHE='financas-v3';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
